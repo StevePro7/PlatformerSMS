f48ea510af3d025affa6223184005aae9e65b3e5
StevePro7 <steveprozune@hotmail.com>
2019-01-14 21:30:32 +0000

    Add test sprites

--- dev/gfx.h
+++ dev/gfx.h
@@ -7,3 +7,21 @@ extern const unsigned char	back_tiles__tilemap__bin[];
 extern const unsigned char	back_tiles__tiles__psgcompr[];
 #define				back_tiles__tiles__psgcompr_size 1827
 
+extern const unsigned char	EnemyA_Idle_01__tiles__psgcompr[];
+#define				EnemyA_Idle_01__tiles__psgcompr_size 139
+
+extern const unsigned char	EnemyB_Idle_01__tiles__psgcompr[];
+#define				EnemyB_Idle_01__tiles__psgcompr_size 154
+
+extern const unsigned char	EnemyC_Idle_01__tiles__psgcompr[];
+#define				EnemyC_Idle_01__tiles__psgcompr_size 115
+
+extern const unsigned char	EnemyD_Idle_01__tiles__psgcompr[];
+#define				EnemyD_Idle_01__tiles__psgcompr_size 155
+
+extern const unsigned char	Player_Idle__tiles__psgcompr[];
+#define				Player_Idle__tiles__psgcompr_size 174
+
+extern const unsigned char	Sprites__palette__bin[];
+#define				Sprites__palette__bin_size 16
+
--- dev/image.bat
+++ dev/image.bat
@@ -7,8 +7,14 @@ REM Tiles
 REM hard code palette to ensure first is 0x00 and last is 0x3f
 REM .db $00 $01 $05 $06 $06 $07 $05 $07 $07 $17 $1A $2A $0A $1F $0F $3F
 REM bmp2tile.exe Tiles\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -removedupes -planar -tileoffset  0 -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette
-bmp2tile.exe Tiles\back_tiles.bmp -savetiles "back_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -savetilemap "back_tiles (tilemap).bin" -savepalette "back_tiles (palette).bin" -fullpalette
+bmp2tile.exe Tiles\back_tiles.bmp -savetiles "back_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -savetilemap "back_tiles (tilemap).bin" -savepalette "back_tiles (palette).bin" -fullpalette -exit
 
+REM Sprites
+bmp2tile.exe Sprites\Player_Idle.bmp -savetiles "Player_Idle (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -savepalette "Sprites (palette).bin" -fullpalette  -spritepalette -exit
+bmp2tile.exe Sprites\EnemyA_Idle_01.bmp -savetiles "EnemyA_Idle_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+bmp2tile.exe Sprites\EnemyB_Idle_01.bmp -savetiles "EnemyB_Idle_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+bmp2tile.exe Sprites\EnemyC_Idle_01.bmp -savetiles "EnemyC_Idle_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+bmp2tile.exe Sprites\EnemyD_Idle_01.bmp -savetiles "EnemyD_Idle_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
 cd ..\dev
 
 folder2c ..\gfx gfx
