0da65460320754fce742c752fdcb49921a67e2a8
StevePro7 <steveprozune@hotmail.com>
2019-03-04 19:57:25 +0000

    Include master sprite palette

--- dev/entity.bat
+++ dev/entity.bat
@@ -18,9 +18,17 @@ bmp2tile.exe raw\Idle.png -savetiles "enemyA_idle (tiles).psgcompr" -noremovedup
 bmp2tile.exe raw\Run_Left_01.png -savetiles "enemyA_run_left_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
 bmp2tile.exe raw\Run_Left_02.png -savetiles "enemyA_run_left_02 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
 
-cd ..\
+cd ..
 folder2c enemyA enemyA
 
 
 REM Palette
 folder2c master master
+
+
+REM build
+sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 master.c
+
+del *.asm > nul
+del *.lst > nul
+del *.sym > nul
