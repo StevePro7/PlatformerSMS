3c313d4bf620cb688cd66f1eac385e2049a3f328
StevePro7 <steveprozune@hotmail.com>
2019-03-04 20:24:32 +0000

    Include enemyC

---
+++ dev/sprites/enemyC.h
@@ -0,0 +1,63 @@
+extern const unsigned char	enemyC_idle__tiles__psgcompr[];
+#define				enemyC_idle__tiles__psgcompr_size 273
+
+extern const unsigned char	enemyC_run_left_01__tiles__psgcompr[];
+#define				enemyC_run_left_01__tiles__psgcompr_size 279
+
+extern const unsigned char	enemyC_run_left_02__tiles__psgcompr[];
+#define				enemyC_run_left_02__tiles__psgcompr_size 249
+
+extern const unsigned char	enemyC_run_left_03__tiles__psgcompr[];
+#define				enemyC_run_left_03__tiles__psgcompr_size 259
+
+extern const unsigned char	enemyC_run_left_04__tiles__psgcompr[];
+#define				enemyC_run_left_04__tiles__psgcompr_size 205
+
+extern const unsigned char	enemyC_run_left_05__tiles__psgcompr[];
+#define				enemyC_run_left_05__tiles__psgcompr_size 255
+
+extern const unsigned char	enemyC_run_left_06__tiles__psgcompr[];
+#define				enemyC_run_left_06__tiles__psgcompr_size 281
+
+extern const unsigned char	enemyC_run_left_07__tiles__psgcompr[];
+#define				enemyC_run_left_07__tiles__psgcompr_size 251
+
+extern const unsigned char	enemyC_run_left_08__tiles__psgcompr[];
+#define				enemyC_run_left_08__tiles__psgcompr_size 236
+
+extern const unsigned char	enemyC_run_left_09__tiles__psgcompr[];
+#define				enemyC_run_left_09__tiles__psgcompr_size 215
+
+extern const unsigned char	enemyC_run_left_10__tiles__psgcompr[];
+#define				enemyC_run_left_10__tiles__psgcompr_size 271
+
+extern const unsigned char	enemyC_run_rght_01__tiles__psgcompr[];
+#define				enemyC_run_rght_01__tiles__psgcompr_size 279
+
+extern const unsigned char	enemyC_run_rght_02__tiles__psgcompr[];
+#define				enemyC_run_rght_02__tiles__psgcompr_size 249
+
+extern const unsigned char	enemyC_run_rght_03__tiles__psgcompr[];
+#define				enemyC_run_rght_03__tiles__psgcompr_size 259
+
+extern const unsigned char	enemyC_run_rght_04__tiles__psgcompr[];
+#define				enemyC_run_rght_04__tiles__psgcompr_size 205
+
+extern const unsigned char	enemyC_run_rght_05__tiles__psgcompr[];
+#define				enemyC_run_rght_05__tiles__psgcompr_size 255
+
+extern const unsigned char	enemyC_run_rght_06__tiles__psgcompr[];
+#define				enemyC_run_rght_06__tiles__psgcompr_size 281
+
+extern const unsigned char	enemyC_run_rght_07__tiles__psgcompr[];
+#define				enemyC_run_rght_07__tiles__psgcompr_size 251
+
+extern const unsigned char	enemyC_run_rght_08__tiles__psgcompr[];
+#define				enemyC_run_rght_08__tiles__psgcompr_size 236
+
+extern const unsigned char	enemyC_run_rght_09__tiles__psgcompr[];
+#define				enemyC_run_rght_09__tiles__psgcompr_size 215
+
+extern const unsigned char	enemyC_run_rght_10__tiles__psgcompr[];
+#define				enemyC_run_rght_10__tiles__psgcompr_size 271
+
