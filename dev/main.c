ae9b65f2778c0b61c85b076bd29c218f8af601ae
StevePro7 <steveprozune@hotmail.com>
2019-03-08 15:57:15 +0000

    Tweak main code

--- dev/main.c
+++ dev/main.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 // Global functions.
-//void( *load_method[ MAX_SCREEENS ] )( );
-//void( *update_method[ MAX_SCREEENS ] )( unsigned char *screen_type );
-//void custom_initialize();
+void custom_initialize();
 
 void main(void)
 {
 	// Global variables.
 	static bool global_pause;
-	//static unsigned char curr_screen_type;
-	static unsigned char open_screen_type;
+	unsigned char open_screen_type;
 
 	engine_asm_manager_clear_VRAM();
 	devkit_SMS_init();
@@ -18,15 +15,14 @@ void main(void)
 
 	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
 	devkit_SMS_useFirstHalfTilesforSprites_False();						// true changes tile color in Tile Viewer
-	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );	// TODO remember to uncomment this...!
+	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
 	engine_content_manager_load_sprites();
 	engine_content_manager_load_back_tiles();
 
-	//custom_initialize();
-	//curr_screen_type = screen_type_none;
-	//open_screen_type = screen_type_load;
-	open_screen_type = screen_type_init;
+	custom_initialize();
+	open_screen_type = screen_type_load;
+	//open_screen_type = screen_type_init;
 	//open_screen_type = screen_type_test;
 	//open_screen_type = screen_type_play;
 
@@ -34,17 +30,10 @@ void main(void)
 	devkit_SMS_displayOn();
 	for (;;)
 	{
-		//if( curr_screen_type != next_screen_type )
-		//{
-		//	curr_screen_type = next_screen_type;
-		//	load_method[ curr_screen_type ]();
-		//}
-
 		devkit_SMS_initSprites();
 		engine_input_manager_update();
 
 		engine_screen_manager_update();
-		//update_method[ curr_screen_type ]( &next_screen_type );
 
 		devkit_SMS_finalizeSprites();
 		devkit_SMS_waitForVBlank();
@@ -54,30 +43,6 @@ void main(void)
 
 void custom_initialize()
 {
-	// Set load methods.
-	/*load_method[ screen_type_none ] = screen_none_screen_load;
-	load_method[ screen_type_test ] = screen_test_screen_load;
-	load_method[ screen_type_init ] = screen_init_screen_load;
-	load_method[ screen_type_load ] = screen_load_screen_load;
-	load_method[ screen_type_play ] = screen_play_screen_load;
-	load_method[ screen_type_func ] = screen_func_screen_load;*/
-	/*
-	load_method[ screen_type_load ] = screen_load_screen_load;
-	load_method[ screen_type_splash ] = screen_splash_screen_load;
-	*/
-
-	// Set update methods.
-	//update_method[ screen_type_none ] = screen_none_screen_update;
-	//update_method[ screen_type_test ] = screen_test_screen_update;
-	//update_method[ screen_type_init ] = screen_init_screen_update;
-	//update_method[ screen_type_load ] = screen_load_screen_update;
-	//update_method[ screen_type_play ] = screen_play_screen_update;
-	//update_method[ screen_type_func ] = screen_func_screen_update;
-	/*
-	update_method[ screen_type_load ] = screen_load_screen_update;
-	update_method[ screen_type_splash ] = screen_splash_screen_update;
-	*/
-
 	// Initialize hack manager.
 	engine_hack_manager_init();
 	engine_hack_manager_invert();
