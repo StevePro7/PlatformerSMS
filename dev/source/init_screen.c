a324e7f984ccca8a1ad2b3790dabf88f5065d9f5
StevePro7 <steveprozune@hotmail.com>
2019-03-09 21:10:16 +0000

    Animate player and enemies

--- dev/source/init_screen.c
+++ dev/source/init_screen.c
@@ -12,6 +12,10 @@
 #include "input_manager.h"
 #include "state_manager.h"
 
+static unsigned int index;
+static unsigned char half;
+static void draw();
+
 void screen_init_screen_load()
 {
 	engine_level_manager_load_index( 4 );
@@ -28,20 +32,50 @@ void screen_init_screen_load()
 	//engine_anim_manager_player_load_run( move_type_rght, half_type_prev );
 	//engine_anim_manager_player_load_run( move_type_rght, half_type_next );
 
-	//engine_anim_manager_enemyX_load_move_idle( sprite_type_enemyD );
-	engine_anim_manager_enemyX_load_move_run( sprite_type_enemyA, move_type_left, half_type_next );
+	//engine_anim_manager_enemyX_load_move_idle( sprite_type_enemyA );
+	half = 0;
+	draw();
+	index = 1;
+	
+	engine_font_manager_draw_data( index, 8, 8 );
+	engine_font_manager_draw_data( 340, 8, 9 );
 }
 
 void screen_init_screen_update( unsigned char *screen_type )
 {
-	//unsigned char test;
+	unsigned char test;
+	unsigned int tile;
 
-	/*test = engine_input_manager_hold_left();
+	//tile = 328 + index * 12;			// enemyX
+	tile = 256 + index * 12;			// player
+
+	test = engine_input_manager_hold_left();
 	if( test )
 	{
-		engine_anim_manager_player_load_run_left( half_type_prev );
-	}*/
+		index++;
+		//engine_font_manager_draw_data( index, 8, 8 );
+		if( index >= 6 )
+		{
+			index = 1;
+			//engine_font_manager_draw_data( index, 8, 8 );
+			//engine_font_manager_draw_text( "SUZANNE", 8, 10 );
+			half = 1 - half;
+			engine_font_manager_draw_data( half, 8, 11 );
+			draw();
+		}
 
-	engine_player_manager_draw();
+		//tile = 328 + index * 12;			// enemyX
+		tile = 256 + index * 12;			// player
+		//engine_font_manager_draw_data( tile, 8, 9 );
+	}
+
+	//engine_player_manager_draw();
+	engine_anim_manager_draw(28, 144, tile );
 	*screen_type = screen_type_init;
+}
+
+static void draw()
+{
+	//engine_anim_manager_enemyX_load_move_run( sprite_type_enemyA, move_type_rght, half );
+	engine_anim_manager_player_load_run( move_type_left, half );
 }
