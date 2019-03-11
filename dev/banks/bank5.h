0b8bcd8c2341be7aeeb29702f6345fbf931900e5
StevePro7 <steveprozune@hotmail.com>
2019-03-11 12:52:53 +0000

    Update content

--- dev/banks/bank5.h
+++ dev/banks/bank5.h
@@ -1,4 +1,200 @@
-extern const unsigned char	README05_md[];
-#define				README05_md_size 6
-#define				README05_md_bank 5
+extern const unsigned char	level0601_txt[];
+#define				level0601_txt_size 216
+#define				level0601_txt_bank 5
+
+extern const unsigned char	level0602_txt[];
+#define				level0602_txt_size 216
+#define				level0602_txt_bank 5
+
+extern const unsigned char	level0603_txt[];
+#define				level0603_txt_size 216
+#define				level0603_txt_bank 5
+
+extern const unsigned char	level0604_txt[];
+#define				level0604_txt_size 216
+#define				level0604_txt_bank 5
+
+extern const unsigned char	level0605_txt[];
+#define				level0605_txt_size 216
+#define				level0605_txt_bank 5
+
+extern const unsigned char	level0606_txt[];
+#define				level0606_txt_size 216
+#define				level0606_txt_bank 5
+
+extern const unsigned char	level0607_txt[];
+#define				level0607_txt_size 216
+#define				level0607_txt_bank 5
+
+extern const unsigned char	level0608_txt[];
+#define				level0608_txt_size 216
+#define				level0608_txt_bank 5
+
+extern const unsigned char	level0609_txt[];
+#define				level0609_txt_size 216
+#define				level0609_txt_bank 5
+
+extern const unsigned char	level0610_txt[];
+#define				level0610_txt_size 216
+#define				level0610_txt_bank 5
+
+extern const unsigned char	level0701_txt[];
+#define				level0701_txt_size 216
+#define				level0701_txt_bank 5
+
+extern const unsigned char	level0702_txt[];
+#define				level0702_txt_size 216
+#define				level0702_txt_bank 5
+
+extern const unsigned char	level0703_txt[];
+#define				level0703_txt_size 216
+#define				level0703_txt_bank 5
+
+extern const unsigned char	level0704_txt[];
+#define				level0704_txt_size 216
+#define				level0704_txt_bank 5
+
+extern const unsigned char	level0705_txt[];
+#define				level0705_txt_size 216
+#define				level0705_txt_bank 5
+
+extern const unsigned char	level0706_txt[];
+#define				level0706_txt_size 216
+#define				level0706_txt_bank 5
+
+extern const unsigned char	level0707_txt[];
+#define				level0707_txt_size 216
+#define				level0707_txt_bank 5
+
+extern const unsigned char	level0708_txt[];
+#define				level0708_txt_size 216
+#define				level0708_txt_bank 5
+
+extern const unsigned char	level0709_txt[];
+#define				level0709_txt_size 216
+#define				level0709_txt_bank 5
+
+extern const unsigned char	level0710_txt[];
+#define				level0710_txt_size 216
+#define				level0710_txt_bank 5
+
+extern const unsigned char	level0801_txt[];
+#define				level0801_txt_size 216
+#define				level0801_txt_bank 5
+
+extern const unsigned char	level0802_txt[];
+#define				level0802_txt_size 216
+#define				level0802_txt_bank 5
+
+extern const unsigned char	level0803_txt[];
+#define				level0803_txt_size 216
+#define				level0803_txt_bank 5
+
+extern const unsigned char	level0804_txt[];
+#define				level0804_txt_size 216
+#define				level0804_txt_bank 5
+
+extern const unsigned char	level0805_txt[];
+#define				level0805_txt_size 216
+#define				level0805_txt_bank 5
+
+extern const unsigned char	level0806_txt[];
+#define				level0806_txt_size 216
+#define				level0806_txt_bank 5
+
+extern const unsigned char	level0807_txt[];
+#define				level0807_txt_size 216
+#define				level0807_txt_bank 5
+
+extern const unsigned char	level0808_txt[];
+#define				level0808_txt_size 216
+#define				level0808_txt_bank 5
+
+extern const unsigned char	level0809_txt[];
+#define				level0809_txt_size 216
+#define				level0809_txt_bank 5
+
+extern const unsigned char	level0810_txt[];
+#define				level0810_txt_size 216
+#define				level0810_txt_bank 5
+
+extern const unsigned char	level0901_txt[];
+#define				level0901_txt_size 216
+#define				level0901_txt_bank 5
+
+extern const unsigned char	level0902_txt[];
+#define				level0902_txt_size 216
+#define				level0902_txt_bank 5
+
+extern const unsigned char	level0903_txt[];
+#define				level0903_txt_size 216
+#define				level0903_txt_bank 5
+
+extern const unsigned char	level0904_txt[];
+#define				level0904_txt_size 216
+#define				level0904_txt_bank 5
+
+extern const unsigned char	level0905_txt[];
+#define				level0905_txt_size 216
+#define				level0905_txt_bank 5
+
+extern const unsigned char	level0906_txt[];
+#define				level0906_txt_size 216
+#define				level0906_txt_bank 5
+
+extern const unsigned char	level0907_txt[];
+#define				level0907_txt_size 216
+#define				level0907_txt_bank 5
+
+extern const unsigned char	level0908_txt[];
+#define				level0908_txt_size 216
+#define				level0908_txt_bank 5
+
+extern const unsigned char	level0909_txt[];
+#define				level0909_txt_size 216
+#define				level0909_txt_bank 5
+
+extern const unsigned char	level0910_txt[];
+#define				level0910_txt_size 216
+#define				level0910_txt_bank 5
+
+extern const unsigned char	level1001_txt[];
+#define				level1001_txt_size 216
+#define				level1001_txt_bank 5
+
+extern const unsigned char	level1002_txt[];
+#define				level1002_txt_size 216
+#define				level1002_txt_bank 5
+
+extern const unsigned char	level1003_txt[];
+#define				level1003_txt_size 216
+#define				level1003_txt_bank 5
+
+extern const unsigned char	level1004_txt[];
+#define				level1004_txt_size 216
+#define				level1004_txt_bank 5
+
+extern const unsigned char	level1005_txt[];
+#define				level1005_txt_size 216
+#define				level1005_txt_bank 5
+
+extern const unsigned char	level1006_txt[];
+#define				level1006_txt_size 216
+#define				level1006_txt_bank 5
+
+extern const unsigned char	level1007_txt[];
+#define				level1007_txt_size 216
+#define				level1007_txt_bank 5
+
+extern const unsigned char	level1008_txt[];
+#define				level1008_txt_size 216
+#define				level1008_txt_bank 5
+
+extern const unsigned char	level1009_txt[];
+#define				level1009_txt_size 216
+#define				level1009_txt_bank 5
+
+extern const unsigned char	level1010_txt[];
+#define				level1010_txt_size 216
+#define				level1010_txt_bank 5
 
