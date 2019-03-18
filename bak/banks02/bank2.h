12be86a436254a67df66ddef1a37961343f3266e
StevePro7 <steveprozune@hotmail.com>
2019-03-18 16:16:06 +0000

    Back up banks02

---
+++ bak/banks02/bank2.h
@@ -0,0 +1,104 @@
+extern const unsigned char	enemyA_idle__tiles__psgcompr[];
+#define				enemyA_idle__tiles__psgcompr_size 284
+#define				enemyA_idle__tiles__psgcompr_bank 2
+
+extern const unsigned char	enemyB_idle__tiles__psgcompr[];
+#define				enemyB_idle__tiles__psgcompr_size 314
+#define				enemyB_idle__tiles__psgcompr_bank 2
+
+extern const unsigned char	enemyC_idle__tiles__psgcompr[];
+#define				enemyC_idle__tiles__psgcompr_size 273
+#define				enemyC_idle__tiles__psgcompr_bank 2
+
+extern const unsigned char	enemyD_idle__tiles__psgcompr[];
+#define				enemyD_idle__tiles__psgcompr_size 318
+#define				enemyD_idle__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_idle__tiles__psgcompr[];
+#define				player_idle__tiles__psgcompr_size 302
+#define				player_idle__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_01__tiles__psgcompr[];
+#define				player_run_left_01__tiles__psgcompr_size 285
+#define				player_run_left_01__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_02__tiles__psgcompr[];
+#define				player_run_left_02__tiles__psgcompr_size 280
+#define				player_run_left_02__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_03__tiles__psgcompr[];
+#define				player_run_left_03__tiles__psgcompr_size 255
+#define				player_run_left_03__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_04__tiles__psgcompr[];
+#define				player_run_left_04__tiles__psgcompr_size 224
+#define				player_run_left_04__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_05__tiles__psgcompr[];
+#define				player_run_left_05__tiles__psgcompr_size 276
+#define				player_run_left_05__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_06__tiles__psgcompr[];
+#define				player_run_left_06__tiles__psgcompr_size 289
+#define				player_run_left_06__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_07__tiles__psgcompr[];
+#define				player_run_left_07__tiles__psgcompr_size 285
+#define				player_run_left_07__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_08__tiles__psgcompr[];
+#define				player_run_left_08__tiles__psgcompr_size 257
+#define				player_run_left_08__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_09__tiles__psgcompr[];
+#define				player_run_left_09__tiles__psgcompr_size 233
+#define				player_run_left_09__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_left_10__tiles__psgcompr[];
+#define				player_run_left_10__tiles__psgcompr_size 280
+#define				player_run_left_10__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_01__tiles__psgcompr[];
+#define				player_run_rght_01__tiles__psgcompr_size 285
+#define				player_run_rght_01__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_02__tiles__psgcompr[];
+#define				player_run_rght_02__tiles__psgcompr_size 280
+#define				player_run_rght_02__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_03__tiles__psgcompr[];
+#define				player_run_rght_03__tiles__psgcompr_size 255
+#define				player_run_rght_03__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_04__tiles__psgcompr[];
+#define				player_run_rght_04__tiles__psgcompr_size 224
+#define				player_run_rght_04__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_05__tiles__psgcompr[];
+#define				player_run_rght_05__tiles__psgcompr_size 276
+#define				player_run_rght_05__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_06__tiles__psgcompr[];
+#define				player_run_rght_06__tiles__psgcompr_size 289
+#define				player_run_rght_06__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_07__tiles__psgcompr[];
+#define				player_run_rght_07__tiles__psgcompr_size 285
+#define				player_run_rght_07__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_08__tiles__psgcompr[];
+#define				player_run_rght_08__tiles__psgcompr_size 257
+#define				player_run_rght_08__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_09__tiles__psgcompr[];
+#define				player_run_rght_09__tiles__psgcompr_size 233
+#define				player_run_rght_09__tiles__psgcompr_bank 2
+
+extern const unsigned char	player_run_rght_10__tiles__psgcompr[];
+#define				player_run_rght_10__tiles__psgcompr_size 280
+#define				player_run_rght_10__tiles__psgcompr_bank 2
+
+extern const unsigned char	sprites__palette__bin[];
+#define				sprites__palette__bin_size 16
+#define				sprites__palette__bin_bank 2
+
