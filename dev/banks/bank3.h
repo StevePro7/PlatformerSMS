bf1f90c63e62b275dfd4775fd168e7a951371fe8
StevePro7 <steveprozune@hotmail.com>
2019-03-19 11:30:05 +0000

    Update guard content

--- dev/banks/bank3.h
+++ dev/banks/bank3.h
@@ -1,33 +1,33 @@
 extern const unsigned char	guardA_idle__tilemap__bin[];
-#define				guardA_idle__tilemap__bin_size 24
+#define				guardA_idle__tilemap__bin_size 16
 #define				guardA_idle__tilemap__bin_bank 3
 
 extern const unsigned char	guardA_idle__tiles__psgcompr[];
-#define				guardA_idle__tiles__psgcompr_size 284
+#define				guardA_idle__tiles__psgcompr_size 239
 #define				guardA_idle__tiles__psgcompr_bank 3
 
 extern const unsigned char	guardB_idle__tilemap__bin[];
-#define				guardB_idle__tilemap__bin_size 24
+#define				guardB_idle__tilemap__bin_size 16
 #define				guardB_idle__tilemap__bin_bank 3
 
 extern const unsigned char	guardB_idle__tiles__psgcompr[];
-#define				guardB_idle__tiles__psgcompr_size 314
+#define				guardB_idle__tiles__psgcompr_size 258
 #define				guardB_idle__tiles__psgcompr_bank 3
 
 extern const unsigned char	guardC_idle__tilemap__bin[];
-#define				guardC_idle__tilemap__bin_size 24
+#define				guardC_idle__tilemap__bin_size 16
 #define				guardC_idle__tilemap__bin_bank 3
 
 extern const unsigned char	guardC_idle__tiles__psgcompr[];
-#define				guardC_idle__tiles__psgcompr_size 273
+#define				guardC_idle__tiles__psgcompr_size 245
 #define				guardC_idle__tiles__psgcompr_bank 3
 
 extern const unsigned char	guardD_idle__tilemap__bin[];
-#define				guardD_idle__tilemap__bin_size 24
+#define				guardD_idle__tilemap__bin_size 16
 #define				guardD_idle__tilemap__bin_bank 3
 
 extern const unsigned char	guardD_idle__tiles__psgcompr[];
-#define				guardD_idle__tiles__psgcompr_size 318
+#define				guardD_idle__tiles__psgcompr_size 258
 #define				guardD_idle__tiles__psgcompr_bank 3
 
 extern const unsigned char	splash__tilemap__stmcompr[];
