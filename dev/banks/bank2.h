4bf7a800fed78cfebc432cd58ae28a3bbcca5f24
StevePro7 <steveprozune@hotmail.com>
2019-03-19 21:14:23 +0000

    Reduce enemy sprite size

--- dev/banks/bank2.h
+++ dev/banks/bank2.h
@@ -1,17 +1,17 @@
 extern const unsigned char	enemyA_idle__tiles__psgcompr[];
-#define				enemyA_idle__tiles__psgcompr_size 284
+#define				enemyA_idle__tiles__psgcompr_size 239
 #define				enemyA_idle__tiles__psgcompr_bank 2
 
 extern const unsigned char	enemyB_idle__tiles__psgcompr[];
-#define				enemyB_idle__tiles__psgcompr_size 314
+#define				enemyB_idle__tiles__psgcompr_size 258
 #define				enemyB_idle__tiles__psgcompr_bank 2
 
 extern const unsigned char	enemyC_idle__tiles__psgcompr[];
-#define				enemyC_idle__tiles__psgcompr_size 273
+#define				enemyC_idle__tiles__psgcompr_size 245
 #define				enemyC_idle__tiles__psgcompr_bank 2
 
 extern const unsigned char	enemyD_idle__tiles__psgcompr[];
-#define				enemyD_idle__tiles__psgcompr_size 318
+#define				enemyD_idle__tiles__psgcompr_size 258
 #define				enemyD_idle__tiles__psgcompr_bank 2
 
 extern const unsigned char	player_idle__tiles__psgcompr[];
@@ -38,26 +38,6 @@ extern const unsigned char	player_run_left_05__tiles__psgcompr[];
 #define				player_run_left_05__tiles__psgcompr_size 276
 #define				player_run_left_05__tiles__psgcompr_bank 2
 
-extern const unsigned char	player_run_left_06__tiles__psgcompr[];
-#define				player_run_left_06__tiles__psgcompr_size 289
-#define				player_run_left_06__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_left_07__tiles__psgcompr[];
-#define				player_run_left_07__tiles__psgcompr_size 285
-#define				player_run_left_07__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_left_08__tiles__psgcompr[];
-#define				player_run_left_08__tiles__psgcompr_size 257
-#define				player_run_left_08__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_left_09__tiles__psgcompr[];
-#define				player_run_left_09__tiles__psgcompr_size 233
-#define				player_run_left_09__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_left_10__tiles__psgcompr[];
-#define				player_run_left_10__tiles__psgcompr_size 280
-#define				player_run_left_10__tiles__psgcompr_bank 2
-
 extern const unsigned char	player_run_rght_01__tiles__psgcompr[];
 #define				player_run_rght_01__tiles__psgcompr_size 285
 #define				player_run_rght_01__tiles__psgcompr_bank 2
@@ -77,26 +57,6 @@ extern const unsigned char	player_run_rght_04__tiles__psgcompr[];
 extern const unsigned char	player_run_rght_05__tiles__psgcompr[];
 #define				player_run_rght_05__tiles__psgcompr_size 276
 #define				player_run_rght_05__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_rght_06__tiles__psgcompr[];
-#define				player_run_rght_06__tiles__psgcompr_size 289
-#define				player_run_rght_06__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_rght_07__tiles__psgcompr[];
-#define				player_run_rght_07__tiles__psgcompr_size 285
-#define				player_run_rght_07__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_rght_08__tiles__psgcompr[];
-#define				player_run_rght_08__tiles__psgcompr_size 257
-#define				player_run_rght_08__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_rght_09__tiles__psgcompr[];
-#define				player_run_rght_09__tiles__psgcompr_size 233
-#define				player_run_rght_09__tiles__psgcompr_bank 2
-
-extern const unsigned char	player_run_rght_10__tiles__psgcompr[];
-#define				player_run_rght_10__tiles__psgcompr_size 280
-#define				player_run_rght_10__tiles__psgcompr_bank 2
 
 extern const unsigned char	sprites__palette__bin[];
 #define				sprites__palette__bin_size 16
--- dev/build.bat
+++ dev/build.bat
@@ -26,7 +26,7 @@ REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _sms_
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _snd_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 global_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 debug_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 hack_manager.c
+rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 hack_manager.c
 
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level_object.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 anim_object.c
@@ -35,14 +35,14 @@ REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 conte
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 font_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 text_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 delay_manager.c
-rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 sprite_manager.c
+sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 sprite_manager.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 tile_manager.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 anim_manager.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 input_manager.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 player_manager.c
 sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 enemy_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 stats_manager.c
+::sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 stats_manager.c
 
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 audio_manager.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 game_manager.c
@@ -62,12 +62,12 @@ REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 level
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 init_screen.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 load_screen.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 ready_screen.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 play_screen.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 play_screen.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 reset_screen.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 pass_screen.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 dead_screen.c
+rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 dead_screen.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 cont_screen.c
-rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 over_screen.c
+sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 over_screen.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 beat_screen.c
 rem sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 test_screen.c
 REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 func_screen.c
