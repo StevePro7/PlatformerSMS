3c3702a73851774bbb5c2dceb6b86dc78c9f38d2
StevePro7 <steveprozune@hotmail.com>
2019-03-23 21:08:32 +0000

    Tweak statistics

--- dev/source/stats_manager.c
+++ dev/source/stats_manager.c
@@ -24,15 +24,6 @@ void engine_stats_manager_init()
 	so->enemys_velX[ sprite_type_enemyA ][ 6 ] = 2;
 	so->enemys_velX[ sprite_type_enemyA ][ 7 ] = 2;
 
-	/*so->enemys_velX[ sprite_type_enemyB ][ 0 ] = 1;
-	so->enemys_velX[ sprite_type_enemyB ][ 1 ] = 2;
-	so->enemys_velX[ sprite_type_enemyB ][ 2 ] = 2;
-	so->enemys_velX[ sprite_type_enemyB ][ 3 ] = 2;
-	so->enemys_velX[ sprite_type_enemyB ][ 4 ] = 1;
-	so->enemys_velX[ sprite_type_enemyB ][ 5 ] = 1;
-	so->enemys_velX[ sprite_type_enemyB ][ 6 ] = 1;
-	so->enemys_velX[ sprite_type_enemyB ][ 7 ] = 1;*/
-
 	so->enemys_velX[ sprite_type_enemyC ][ 0 ] = 1;
 	so->enemys_velX[ sprite_type_enemyC ][ 1 ] = 1;
 	so->enemys_velX[ sprite_type_enemyC ][ 2 ] = 1;
@@ -41,15 +32,6 @@ void engine_stats_manager_init()
 	so->enemys_velX[ sprite_type_enemyC ][ 5 ] = 1;
 	so->enemys_velX[ sprite_type_enemyC ][ 6 ] = 1;
 	so->enemys_velX[ sprite_type_enemyC ][ 7 ] = 2;
-
-	/*so->enemys_velX[ sprite_type_enemyD ][ 0 ] = 2;
-	so->enemys_velX[ sprite_type_enemyD ][ 1 ] = 3;
-	so->enemys_velX[ sprite_type_enemyD ][ 2 ] = 2;
-	so->enemys_velX[ sprite_type_enemyD ][ 3 ] = 3;
-	so->enemys_velX[ sprite_type_enemyD ][ 4 ] = 1;
-	so->enemys_velX[ sprite_type_enemyD ][ 5 ] = 1;
-	so->enemys_velX[ sprite_type_enemyD ][ 6 ] = 1;
-	so->enemys_velX[ sprite_type_enemyD ][ 7 ] = 1;*/
 
 
 	// Enemy wait.
@@ -62,14 +44,14 @@ void engine_stats_manager_init()
 	so->enemys_wait[ sprite_type_enemyA ][ 6 ] = 125;
 	so->enemys_wait[ sprite_type_enemyA ][ 7 ] = 125;
 
-	so->enemys_wait[ sprite_type_enemyB ][ 0 ] = 50;
-	so->enemys_wait[ sprite_type_enemyB ][ 1 ] = 100;
-	so->enemys_wait[ sprite_type_enemyB ][ 2 ] = 10;
-	so->enemys_wait[ sprite_type_enemyB ][ 3 ] = 10;
-	so->enemys_wait[ sprite_type_enemyB ][ 4 ] = 15;
-	so->enemys_wait[ sprite_type_enemyB ][ 5 ] = 15;
-	so->enemys_wait[ sprite_type_enemyB ][ 6 ] = 15;
-	so->enemys_wait[ sprite_type_enemyB ][ 7 ] = 15;
+	so->enemys_wait[ sprite_type_enemyB ][ 0 ] = 125;
+	so->enemys_wait[ sprite_type_enemyB ][ 1 ] = 110;
+	so->enemys_wait[ sprite_type_enemyB ][ 2 ] = 100;
+	so->enemys_wait[ sprite_type_enemyB ][ 3 ] = 95;
+	so->enemys_wait[ sprite_type_enemyB ][ 4 ] = 90;
+	so->enemys_wait[ sprite_type_enemyB ][ 5 ] = 85;
+	so->enemys_wait[ sprite_type_enemyB ][ 6 ] = 80;
+	so->enemys_wait[ sprite_type_enemyB ][ 7 ] = 75;
 
 	so->enemys_wait[ sprite_type_enemyC ][ 0 ] = 200;
 	so->enemys_wait[ sprite_type_enemyC ][ 1 ] = 175;
@@ -80,14 +62,14 @@ void engine_stats_manager_init()
 	so->enemys_wait[ sprite_type_enemyC ][ 6 ] = 100;
 	so->enemys_wait[ sprite_type_enemyC ][ 7 ] = 100;
 
-	so->enemys_wait[ sprite_type_enemyD ][ 0 ] = 50;
-	so->enemys_wait[ sprite_type_enemyD ][ 1 ] = 25;
-	so->enemys_wait[ sprite_type_enemyD ][ 2 ] = 10;
-	so->enemys_wait[ sprite_type_enemyD ][ 3 ] = 10;
-	so->enemys_wait[ sprite_type_enemyD ][ 4 ] = 15;
-	so->enemys_wait[ sprite_type_enemyD ][ 5 ] = 15;
-	so->enemys_wait[ sprite_type_enemyD ][ 6 ] = 15;
-	so->enemys_wait[ sprite_type_enemyD ][ 7 ] = 15;
+	so->enemys_wait[ sprite_type_enemyD ][ 0 ] = 100;
+	so->enemys_wait[ sprite_type_enemyD ][ 1 ] = 90;
+	so->enemys_wait[ sprite_type_enemyD ][ 2 ] = 80;
+	so->enemys_wait[ sprite_type_enemyD ][ 3 ] = 80;
+	so->enemys_wait[ sprite_type_enemyD ][ 4 ] = 70;
+	so->enemys_wait[ sprite_type_enemyD ][ 5 ] = 70;
+	so->enemys_wait[ sprite_type_enemyD ][ 6 ] = 60;
+	so->enemys_wait[ sprite_type_enemyD ][ 7 ] = 50;
 
 
 	// Enemy walk - if 0 then continuous walk.
@@ -104,10 +86,10 @@ void engine_stats_manager_init()
 	so->enemys_walk[ sprite_type_enemyC ][ 1 ] = 64;
 	so->enemys_walk[ sprite_type_enemyC ][ 2 ] = 32;
 	so->enemys_walk[ sprite_type_enemyC ][ 3 ] = 32;
-	so->enemys_walk[ sprite_type_enemyC ][ 4 ] = 16;
-	so->enemys_walk[ sprite_type_enemyC ][ 5 ] = 16;
+	so->enemys_walk[ sprite_type_enemyC ][ 4 ] = 24;
+	so->enemys_walk[ sprite_type_enemyC ][ 5 ] = 24;
 	so->enemys_walk[ sprite_type_enemyC ][ 6 ] = 16;
-	so->enemys_walk[ sprite_type_enemyC ][ 7 ] = 8;
+	so->enemys_walk[ sprite_type_enemyC ][ 7 ] = 16;
 
 
 	// EnemyB and EnemyD have similar properties.
@@ -119,18 +101,5 @@ void engine_stats_manager_init()
 		so->enemys_walk[ sprite_type_enemyB ][ idx ] = 0;
 		so->enemys_walk[ sprite_type_enemyD ][ idx ] = 0;
 	}
-}
 
-//unsigned char *count_text[] =
-//{
-//	"01",
-//	"02",
-//	"03",
-//	"04",
-//	"05",
-//	"06",
-//	"07",
-//	"08",
-//	"09",
-//	"10",
-//};
+}
