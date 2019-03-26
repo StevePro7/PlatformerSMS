fc9dab5eda3bdf6877907e208b98869b5158d854
StevePro7 <steveprozune@hotmail.com>
2019-03-26 07:40:09 +0000

    Update music banks

---
+++ dev/banks/bank6.h
@@ -0,0 +1,4 @@
+extern const unsigned char	mus_over_psg[];
+#define				mus_over_psg_size 881
+#define				mus_over_psg_bank 6
+
---
+++ dev/banks/bank7.h
@@ -0,0 +1,4 @@
+extern const unsigned char	mus_beat_psg[];
+#define				mus_beat_psg_size 1573
+#define				mus_beat_psg_bank 7
+
