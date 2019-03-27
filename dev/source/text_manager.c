d8f5720fa566229ae188268686eca8152262af1c
StevePro7 <steveprozune@hotmail.com>
2019-03-27 16:27:45 +0000

    Additional tutorial text

--- dev/source/text_manager.c
+++ dev/source/text_manager.c
@@ -53,15 +53,55 @@ void engine_text_manager_write_info( unsigned char round )
 	unsigned char x, y = 5;
 	x = 22;
 	y = 4;
+
 	if( 9 == round )
 	{
 		engine_font_manager_draw_text( "HINT: HOLD FIRE2 TO RESET", 2, y + 0 );
-		//engine_font_manager_draw_text( "HINT:", 2, y + 0 );
-		//engine_font_manager_draw_text( "HOLD FIRE2 TO RESET", 2, y + 1 );
-		//engine_font_manager_draw_text( "HINT: HOLD", x, y + 0 );
-		//engine_font_manager_draw_text( "HOLD", x, y + 1 );
-		//engine_font_manager_draw_text( "FIRE2 WILL ", x, y + 1);
-		//engine_font_manager_draw_text( "WILL", x, y + 3);
-		//engine_font_manager_draw_text( "RESET", x, y + 2 );
 	}
+	/*
+	else if( 0 == round )
+	{
+		engine_font_manager_draw_text( "WELCOME HERO!", 2, y + 0 );
+		engine_font_manager_draw_text( "HURRY TO THE EXIT!", 2, y + 1 );
+		engine_font_manager_draw_text( "EXIT", 20, y + 20 );
+	}
+	else if( 1 == round )
+	{
+		engine_font_manager_draw_text( "JUMP ON BLOCKS", 2, y + 0 );
+		engine_font_manager_draw_text( "WALK THROUGH PLATFORMS", 2, y + 1 );
+	}
+	else if( 2 == round )
+	{
+		engine_font_manager_draw_text(  "COLLECT THE GEMS", 2, y + 0 );
+	}
+	else if( 3 == round )
+	{
+		engine_font_manager_draw_text( "AVOID THE PITS", 2, y + 0 );
+	}
+	else if( 4 == round )
+	{
+		engine_font_manager_draw_text( "ENEMY DAVE", 2, y + 0 );
+		engine_font_manager_draw_text( "SLOW PACED AND PAUSES", 2, y + 1 );
+	}
+	else if( 5 == round )
+	{
+		engine_font_manager_draw_text( "ENEMY ALEX", 2, y + 0 );
+		engine_font_manager_draw_text( "MID-PACED AND CONSTANT", 2, y + 1 );
+	}
+	else if( 6 == round )
+	{
+		engine_font_manager_draw_text( "ENEMY MIKE", 2, y + 0 );
+		engine_font_manager_draw_text( "MID-PACED BUT PAUSES", 2, y + 1 );
+	}
+	else if( 7 == round )
+	{
+		engine_font_manager_draw_text( "ENEMY EDDIE", 2, y + 0 );
+		engine_font_manager_draw_text( "FAST PACED AND CONSTANT", 2, y + 1 );
+	}
+	else if( 8 == round )
+	{
+		//engine_font_manager_draw_text( "ENEMY EDDIE", 2, y + 0 );
+		engine_font_manager_draw_text( "GUARDS DO NOT MOVE", 2, y + 1 );
+	}
+	*/
 }
