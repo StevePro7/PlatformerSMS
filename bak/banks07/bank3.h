3409564b8defd657b71922318bed4215bf1e2a6a
StevePro7 <steveprozune@hotmail.com>
2019-03-27 08:19:50 +0000

    Back up files

---
+++ bak/banks07/bank3.h
@@ -0,0 +1,56 @@
+extern const unsigned char	guardA_idle__tilemap__bin[];
+#define				guardA_idle__tilemap__bin_size 16
+#define				guardA_idle__tilemap__bin_bank 3
+
+extern const unsigned char	guardA_idle__tiles__psgcompr[];
+#define				guardA_idle__tiles__psgcompr_size 239
+#define				guardA_idle__tiles__psgcompr_bank 3
+
+extern const unsigned char	guardB_idle__tilemap__bin[];
+#define				guardB_idle__tilemap__bin_size 16
+#define				guardB_idle__tilemap__bin_bank 3
+
+extern const unsigned char	guardB_idle__tiles__psgcompr[];
+#define				guardB_idle__tiles__psgcompr_size 258
+#define				guardB_idle__tiles__psgcompr_bank 3
+
+extern const unsigned char	guardC_idle__tilemap__bin[];
+#define				guardC_idle__tilemap__bin_size 16
+#define				guardC_idle__tilemap__bin_bank 3
+
+extern const unsigned char	guardC_idle__tiles__psgcompr[];
+#define				guardC_idle__tiles__psgcompr_size 245
+#define				guardC_idle__tiles__psgcompr_bank 3
+
+extern const unsigned char	guardD_idle__tilemap__bin[];
+#define				guardD_idle__tilemap__bin_size 16
+#define				guardD_idle__tilemap__bin_bank 3
+
+extern const unsigned char	guardD_idle__tiles__psgcompr[];
+#define				guardD_idle__tiles__psgcompr_size 258
+#define				guardD_idle__tiles__psgcompr_bank 3
+
+extern const unsigned char	splash__tilemap__stmcompr[];
+#define				splash__tilemap__stmcompr_size 71
+#define				splash__tilemap__stmcompr_bank 3
+
+extern const unsigned char	splash__tiles__psgcompr[];
+#define				splash__tiles__psgcompr_size 2089
+#define				splash__tiles__psgcompr_bank 3
+
+extern const unsigned char	title6__tilemap__stmcompr[];
+#define				title6__tilemap__stmcompr_size 369
+#define				title6__tilemap__stmcompr_bank 3
+
+extern const unsigned char	title6__tiles__psgcompr[];
+#define				title6__tiles__psgcompr_size 4557
+#define				title6__tiles__psgcompr_bank 3
+
+extern const unsigned char	title8__tilemap__stmcompr[];
+#define				title8__tilemap__stmcompr_size 369
+#define				title8__tilemap__stmcompr_bank 3
+
+extern const unsigned char	title8__tiles__psgcompr[];
+#define				title8__tiles__psgcompr_size 4557
+#define				title8__tiles__psgcompr_bank 3
+
