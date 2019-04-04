f6e1c6dd98a07689c219eee09461864cdada27bb
StevePro7 <steveprozune@hotmail.com>
2019-04-04 21:36:42 +0100

    Update build information

--- dev/source/info_manager.h
+++ dev/source/info_manager.h
@@ -4,11 +4,11 @@
 #define productCode	9999
 #define revision	0
 
-#define verMaj		1
+#define verMaj		2
 #define verMin		0
 #define dateYear	2019
-#define dateMonth	3
-#define dateDay		27
+#define dateMonth	4
+#define dateDay		4
 #define author		"StevePro Studios"
 #define name		"Platformer Explorer"
 #define descr		"Platformer Explorer game for the SMS Power! 2019 Competition"
